5680111d4b40cb1aefc9711d1097a509ed8d0ccc
user.name <gmail@hdknr.com>
2024-09-06 09:28:23 +0900

    get-doc コマンド

--- bin/fig.py
+++ bin/fig.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python
 import click
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+from figma_files import rest
+import json
 
 
 @click.group()
 @click.pass_context
 def group(ctx):
     ctx.ensure_object(dict)
-
-
-if __name__ == "__main__":
-    group()
+    load_dotenv()
 
 
 @group.command()
 @click.argument("file_key")
-@click.open_file("--out", "-o", default=None)
+@click.option("--output", "-o", default=None)
 @click.pass_context
-def get_document(ctx, file_key, out):
+def get_doc(ctx, file_key, output):
     """ドキュメント取得"""
-    out = out or f"/tmp/{file_key}"
-    Path.mkdir(out, exist_ok=True)
-    path = out / "document.json"
+    output = output or f"/tmp/{file_key}"
+    Path(output).mkdir(exist_ok=True)
+
+    document, images = None, None
+    response = rest.get_file(file_key, os.getenv("TOKEN"))
+    if response.status_code == 200:
+        path = Path(output) / "document.json"
+        document = response.json()
+        with open(path, "w") as out:
+            json.dump(document, out, indent=2, ensure_ascii=False)
+
+    response = rest.get_images(file_key, os.getenv("TOKEN"))
+    if response.status_code == 200:
+        path = Path(output) / "images.json"
+        images = response.json()
+        with open(path, "w") as out:
+            json.dump(images, out, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    group()
