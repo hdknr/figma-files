07748a0350664e245143932d1c8ba870cb3d1b85
user.name <gmail@hdknr.com>
2024-09-10 10:43:52 +0900

    Text: p

--- figma_files/node/node.py
+++ figma_files/node/node.py
@@ -3,6 +3,7 @@
 from pydantic import BaseModel
 from typing import Optional
 from lxml import etree
+from .utils import to_snake
 
 
 class Node(BaseModel):
@@ -12,10 +13,14 @@ class Node(BaseModel):
     visible: Optional[bool] = None
     rotation: Optional[float] = None
 
-    def to_element(self, parent, tag="div"):
-        attrib = {
+    @property
+    def html_attrs(self):
+        return {
             "id": self.id,
             "name": self.name,
+            "figma_type": to_snake(self.__class__.__name__),
         }
-        elm: etree._Element = etree.SubElement(parent, tag, attrib=attrib)
+
+    def to_element(self, parent, tag="div"):
+        elm: etree._Element = etree.SubElement(parent, tag, attrib=self.html_attrs)
         return elm
--- figma_files/node/text.py
+++ figma_files/node/text.py
@@ -1,6 +1,7 @@
 from .vector import Vector
 from ..base.primitives import TypeStyle
 from typing import Optional, List, Literal
+from lxml import etree
 
 
 class Text(Vector):
@@ -10,3 +11,14 @@ class Text(Vector):
     styleOverrideTable: Optional[dict] = None
     lineTypes: Optional[List[Literal["ORDERED", "UNORDERED", "NONE"]]]
     lineIndentations: Optional[List[float]]
+
+    @property
+    def html_attrs(self):
+        attrs = super().html_attrs
+        attrs.pop("name")
+        return attrs
+
+    def to_element(self, parent, tag="p"):
+        elm: etree._Element = etree.SubElement(parent, tag, attrib=self.html_attrs)
+        elm.text = self.name
+        return elm
