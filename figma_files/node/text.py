c1d90f28eadf862eb7f5e5a94e29ad4b4f6b65f9
user.name <gmail@hdknr.com>
2024-09-10 17:26:46 +0900

    css (案)

--- figma_files/node/text.py
+++ figma_files/node/text.py
@@ -2,6 +2,7 @@ from .vector import Vector
 from ..base.primitives import TypeStyle
 from typing import Optional, List, Literal
 from lxml import etree
+from cssutils.css import CSSStyleRule as Rule
 
 
 class Text(Vector):
@@ -12,13 +13,23 @@ class Text(Vector):
     lineTypes: Optional[List[Literal["ORDERED", "UNORDERED", "NONE"]]]
     lineIndentations: Optional[List[float]]
 
+    def css(self, sheet, tag):
+        if not self.style:
+            return
+
+        sel = f"{tag}#{self.sanitized_id}"
+        rule = Rule(selectorText=sel)
+        self.style.set_rule(rule)
+        sheet.cssRules.append(rule)
+
     @property
     def html_attrs(self):
         attrs = super().html_attrs
         attrs.pop("name")
         return attrs
 
-    def to_element(self, parent, tag="p"):
+    def to_element(self, parent, sheet, tag="p"):
         elm: etree._Element = etree.SubElement(parent, tag, attrib=self.html_attrs)
         elm.text = self.name
+        # self.css(sheet, tag)
         return elm
