5308b71f352e8aa4c4ce7be52ce698cd114266db
user.name <gmail@hdknr.com>
2024-09-27 13:53:06 +0900

    comment

--- figma_files/base/property_types.py
+++ figma_files/base/property_types.py
@@ -110,16 +110,34 @@ class ImageFilters(BaseModel):
 
 
 class Paint(BaseModel):
+    """
+    Paints in Figma
+    https://help.figma.com/hc/en-us/articles/360041003694-Paints-in-Figma
+    """
+
     type: Literal[
+        # 単色またはペイント
         "SOLID",
+        # 線形(グラデーション):直線上の2つの色間の漸進的な遷移。線の角度を選べます。
         "GRADIENT_LINEAR",
+        # 放射状(グラデーション):中央に色があり、エッジで別の色に遷移する円形のグラデーション。
+        # これは、別の色であったり、透明にフェードしたりする可能性があります。
         "GRADIENT_RADIAL",
+        # 角度(グラデーション):開始位置から時計回りにグラデーションを作成します。
+        # グラデーション内の両方の色の位置を調整して、よりソフトな角度またはより厳しい角度を作成できます。
         "GRADIENT_ANGULAR",
+        # ひし形(グラデーション):オブジェクトまたはレイヤーの中央から始まる4つのポイントを持つグラデーション。
+        # グラデーションの幅と高さは個別に調整できます。
         "GRADIENT_DIAMOND",
+        # 画像または GIF: 静止画像またはアニメーション GIF。
+        # https://help.figma.com/hc/en-us/articles/360040028034-Add-Images-to-your-designs
         "IMAGE",
         "EMOJI",
+        # ビデオ: .mp4、.mov、または .webm ビデオ ファイルをアップロードします。
+        # https://help.figma.com/hc/en-us/articles/8878274530455-Use-videos-in-prototypes
         "VIDEO",
     ]
+
     visible: Optional[bool] = True
     opacity: Optional[float] = 1
     color: Optional[Color] = None
@@ -141,21 +159,43 @@ class LayoutConstraint(BaseModel):
     horizontal: Literal["LEFT", "RIGHT", "CENTER", "LEFT_RIGHT", "SCALE"]
 
 
-EasingType = Literal[
-    "EASE_IN", "EASE_OUT", "EASE_IN_AND_OUT", "LINEAR", "GENTLE_SPRING"
-]
+EasingType = Literal["EASE_IN", "EASE_OUT", "EASE_IN_AND_OUT", "LINEAR", "GENTLE_SPRING"]
 
 
 class LayoutGrid(BaseModel):
+    """Guides to align and place objects within a frame"""
+
     pattern: Literal["COLUMNS", "ROWS", "GRID"]
-    sectionSizeN: float
+    # Orientation of the grid as a string enum
+
+    sectionSize: float
+    # Width of column grid or height of row grid or square grid spacing
+
     visible: bool
+    # Is the grid currently visible?
+
     color: Color
+    # Color of the grid
+
+    # The following properties are only meaningful for directional grids (COLUMNS or ROWS)
+
     alignment: Literal["MIN", "STRETCH", "CENTER"]
+    # Positioning of grid as a string enum
+    # MIN: Grid starts at the left or top of the frame
+    # STRETCH: Grid is stretched to fit the frame
+    # CENTER: Grid is center aligned
+
     gutterSize: float
+    # Spacing in between columns and rows
+
     offset: float
+    # Spacing before the first column or row
+
     count: float
+    # Number of columns or rows
+
     boundVariables: Optional[dict] = None
+    # A mapping of field to the VariableAlias of the bound variable.
 
 
 class Effect(BaseModel):
@@ -238,11 +278,7 @@ class TypeStyle(BaseModel):
     italic: Optional[bool] = None
     fontWeight: Optional[float] = None
     fontSize: Optional[float] = None
-    textCase: Optional[
-        Literal[
-            "ORIGINAL", "UPPER", "LOWER", "TITLE", "SMALL_CAPS", "SMALL_CAPS_FORCED"
-        ]
-    ] = "ORIGINAL"
+    textCase: Optional[Literal["ORIGINAL", "UPPER", "LOWER", "TITLE", "SMALL_CAPS", "SMALL_CAPS_FORCED"]] = "ORIGINAL"
     textDecoration: Optional[
         Literal[
             "NONE",
@@ -250,9 +286,7 @@ class TypeStyle(BaseModel):
             "UNDERLINE",
         ]
     ] = "NONE"
-    textAutoResize: Optional[
-        Literal["NONE", "HEIGHT", "WIDTH_AND_HEIGHT", "WIDTH_AND_HEIGHT"]
-    ] = "NONE"
+    textAutoResize: Optional[Literal["NONE", "HEIGHT", "WIDTH_AND_HEIGHT", "WIDTH_AND_HEIGHT"]] = "NONE"
     textTruncation: Optional[
         Literal[
             "DISABLED",
@@ -260,9 +294,7 @@ class TypeStyle(BaseModel):
         ]
     ] = "DISABLED"
     maxLines: Optional[float] = None
-    textAlignHorizontal: Optional[Literal["LEFT", "RIGHT", "CENTER", "JUSTIFIED"]] = (
-        None
-    )
+    textAlignHorizontal: Optional[Literal["LEFT", "RIGHT", "CENTER", "JUSTIFIED"]] = None
     textAlignVertical: Optional[
         Literal[
             "TOP",
